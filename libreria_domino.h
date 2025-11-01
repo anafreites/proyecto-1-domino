4c54fb3dfc2951ac0bf6cc89bc4a9ac26002d0a0
Anabella Freites <afreites.25@est.ucab.edu.ve>
2025-11-01 19:50:05 -0400

    struct mesa, planteamiento de varias funciones

--- libreria_domino.h
+++ libreria_domino.h
@@ -1,4 +1,4 @@
-//estructura de datos para las fichas
+//estructura de datos para las fichas (pila)
 struct Piedra{
     int valor1;
     int valor2;
@@ -12,8 +12,90 @@ struct Jugador{
     Jugador* sigJugador;
 };
 
-//estructura de datos para el pozo (boneyard)
+//estructura de datos para la mesa (lista ciclica doblemente enlazada)
+struct Mesa{
+    Piedra piece;
+    Mesa* sigFicha;
+    Mesa* antFicha;
+};
+
+//estructura de datos para el pozo (pila)
 struct Pozo{
     Piedra* piedras;
     int num_piedras;
 };
+
+//primitivas de la libreria
+//inicializar el pozo al inicio de una partida
+void inicializarPozo(Pozo* boneyard){
+    
+}
+
+//inicializar la mesa al inicio de una partida
+void inicializarMesa(Mesa** inicioMesa){
+
+}
+
+//inicializar los jugadores
+void inicializarJugadores(Jugador** players, int num_jugadores){
+
+}
+
+//repartir las piedras entre los jugadores al inicio de una partida
+void repartirPiedras(Jugador** player, Piedra piece){
+    
+}
+
+//buscar la piedra mas alta en la mano de un jugador
+Jugador* buscarPiedraMasAlta(Jugador* player){
+    return nullptr;
+}
+
+//agarrar una piedra del pozo cuando un jugador no puede jugar
+void agarrarPiedraDelPozo(Pozo* boneyard, Jugador* player){
+    
+}
+
+//verificar si una piedra se puede jugar
+bool jugadaValida(Mesa* table, Piedra piece){
+    return false;
+}
+
+//recorre todas las piedras de un jugador para saber si puede jugar una ficha
+bool puedeJugar(Jugador* player, Mesa* table){
+    return false;
+}
+
+//saltarse un turno en caso de que no se pueda jugar nada
+void pasarTurno(Jugador** currentPlayer){
+
+}
+
+//buscar y jugar una piedra de la mano del jugador
+Piedra* jugarPiedra(Jugador* player, Piedra piece){
+    return nullptr;
+}
+
+//colocar la piedra jugada en la mesa
+void colocarPiedraEnMesa(Mesa** inicioMesa, Mesa** finMesa, Piedra piece){
+    
+}
+
+//contar los puntos en la mano de un jugador
+int contarPuntosEnMano(Jugador* player){
+    return 0;
+}
+
+Jugador* determinarGanador(Jugador* players, int num_jugadores){
+    return nullptr;
+}
+
+//sumar puntos al jugador que gana la ronda
+int sumarPuntos(Jugador* player, int puntos){
+    return 0;   
+}
+
+//saber si ya el juego termino
+bool juegoTerminado(Jugador* players, Pozo* boneyard){
+    return false;
+}
