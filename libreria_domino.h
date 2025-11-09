23f36224042a50d397d0f0d66a965682ee9d2bf0
German Alfonzo <germanmatheo@gmail.com>
2025-11-08 21:44:12 -0400

    funciones para el pozo

--- libreria_domino.h
+++ libreria_domino.h
@@ -67,3 +67,47 @@ void mostrarPiedra(Piedra p) {
         cout << "[" << p.valor1 << "|" << p.valor2 << "]";
     }
 }
+
+//lfunciones del pozo
+void inicializarPozo(Pozo* boneyard) {
+    boneyard->tope = nullptr;
+    boneyard->num_piedras = 0;
+}
+
+void insertarEnPozo(Pozo* boneyard, Piedra piece) {
+    NodoPozo* nuevoNodo = new NodoPozo;
+    nuevoNodo->ficha = piece;
+    nuevoNodo->siguiente = boneyard->tope;
+    boneyard->tope = nuevoNodo;
+    boneyard->num_piedras++;
+}
+
+Piedra sacarPiedraDelPozo(Pozo* boneyard) {
+    if (boneyard->tope == nullptr) {
+        return { -1, -1, false };
+    }
+    NodoPozo* nodoATomar = boneyard->tope;
+    Piedra fichaTomada = nodoATomar->ficha;
+    boneyard->tope = boneyard->tope->siguiente;
+    boneyard->num_piedras--;
+    delete nodoATomar;
+    return fichaTomada;
+}
+
+void llenarPozo(Pozo* boneyard) {
+    Piedra todasLasFichas[28];
+    int k = 0;
+    for (int i = 0; i <= 6; ++i) {
+        for (int j = i; j <= 6; ++j) {
+            todasLasFichas[k++] = {i, j, false};
+        }
+    }
+    srand(time(0)); 
+    for (int i = 0; i < 28; ++i) {
+        int rand_index = rand() % (28 - i) + i;
+        swapPiedra(&todasLasFichas[i], &todasLasFichas[rand_index]);
+    }
+    for (int i = 0; i < 28; ++i) {
+        insertarEnPozo(boneyard, todasLasFichas[i]);
+    }
+}
